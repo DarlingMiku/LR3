e808531076f98047ad20f767c8ee3b9f9baa8191
Mlku <stonyaleks@gmail.com>
2026-03-28 11:41:33 +0300

    init proj

---
+++ main.py
@@ -0,0 +1,10 @@
+from models.transaction import Transaction
+from models.category import Category
+
+if __name__ == "__main__":
+    t1 = Transaction(100, "Food", "2023-10-25")
+    print(t1)
+
+if __name__ == "__main__":
+    t1 = Category("Food", "Category for food expenses")
+    print(t1)
